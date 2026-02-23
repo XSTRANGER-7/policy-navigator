477ab7a8f511c811b3299bcb13b9efe278e93aaa
Sarthak Gupta <smartsarthakgupta12345@gmail.com>
2026-02-23 18:53:30 +0530

    added py file for deploy

---
+++ zyndai_agent/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="zyndai_agent",
+    version="0.0.1",
+    description="Stub zyndai_agent for Policy Navigator (deployment shim)",
+    packages=find_packages(),
+    include_package_data=True,
+)
